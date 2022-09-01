8eca585e81eddbf3749e3c8426aba42ceac93b0c
nick-ras <nickras@mail.dk>
2022-09-01 17:25:55 +0200

    made open work and got an output, but need to exec second cmd

--- pipex/main.c
+++ pipex/main.c
@@ -11,11 +11,31 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-char	*get_path(char **argv, char **envp)
+char **commands(char **argv)
 {
-	int		i;
-	int		j;
-	char	**paths;
+	char *arr1;
+	char *joined;
+	char **split;
+
+	arr1 = malloc(sizeof(argv[2] + 2));
+	ft_strlcpy(arr1, argv[2], (int)ft_strlen(argv[2]));
+	arr1[ft_strlen(argv[2]) - 1] = ' ';
+	arr1[ft_strlen(argv[2])] = '\0';
+	joined = ft_strjoin(arr1, argv[3]);
+	split = ft_split(joined, ' ');
+
+	free(arr1);
+	arr1 = NULL;
+	free(joined);
+	joined = NULL;
+	return (split);
+}
+
+char *get_path_cmd1(char **cmds, char **envp)
+{
+	int i;
+	int j;
+	char **paths;
 
 	i = 0;
 	j = 0;
@@ -26,9 +46,11 @@ char	*get_path(char **argv, char **envp)
 			paths = ft_split(envp[i], ':');
 			while (paths[j])
 			{
-				if (access(ft_strjoin(paths[j], ft_strjoin("/", argv[1])), \
-				R_OK) == 0)
-					return (ft_strjoin(paths[j], ft_strjoin("/", argv[1])));
+				if (access(ft_strjoin(paths[j], ft_strjoin("/", cmds[0])), R_OK) == 0)
+				{
+					// To do: free path arrays!
+					return (ft_strjoin(paths[j], ft_strjoin("/", cmds[0])));
+				}
 				j++;
 			}
 		}
@@ -36,19 +58,19 @@ char	*get_path(char **argv, char **envp)
 	}
 }
 
-void	execute(char **argv, char **envp,char *path)
+void execute(char **cmds, char **envp, char *path)
 {
 	char *options[3] = {NULL, NULL, NULL};
 	char *arr[50];
 
-	if (ft_strncmp(argv[1], "ls", ft_strlen("ls")) == 0)
+	if (ft_strncmp(cmds[0], "ls", ft_strlen("ls")) == 0)
 	{
-		options[0] = argv[1];
-		if (argv[2])
-			options[1] = argv[2];
+		options[0] = cmds[0];
+		if (cmds[1])
+			options[1] = cmds[1];
 		if (execve(path, options, envp) == -1)
 		{
-			perror(ft_strjoin(path, ft_strjoin("/", argv[1])));
+			perror(ft_strjoin(path, ft_strjoin("/", cmds[1])));
 			exit(1);
 		}
 		else
@@ -56,63 +78,91 @@ void	execute(char **argv, char **envp,char *path)
 	}
 }
 
-int	main(int argc, char **argv, char **envp)
-{
-	char	*path;
-	pid_t	pid1;
-	int     ends[2];
-	int     reader;
-	char	secret[400];
-	char    buffer[400];
 
+int main(int argc, char **argv, char **envp)
+{
+	char *path1;
+	char **cmds;
+	char *buffer[400];
+	pid_t pid1;
+	int pipefd[2];
+	int reader;
+
+	cmds = commands(argv);
 	if (!envp)
 	{
 		ft_printf("could not find envp!\n");
 		exit(1);
 	}
-	path = get_path(argv, envp);
-	// Create a pipe and error check
-	if (pipe(ends) == -1)
-			return (1);
-	// Create a child process
+
+
+
+
+
+
+	path1 = get_path_cmd1(cmds, envp); // gets full path of ls program
+	if (pipe(pipefd) == -1) // Create a pipe and error check
+		return (1);
+
+
+
+	pipefd[0] = open(argv[1], O_RDONLY);
+	pipefd[1] = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 744);
+	if (pipefd[0] < 0 || pipefd[1] < 0)
+		return (-1);
+
+
 	pid1 = fork();
 	if (pid1 == -1)
 	{
-		perror("pid1 is -1");
+		perror("fork function error\n"); // will also print errno
 		exit(1);
 	}
 	else if (pid1 == 0)
 	{
-		// Child process
-		dup2 (ends[1], STDOUT_FILENO);
-		close(ends[0]);
-		close(ends[1]);  // Done writing, close pipe's write end.
-		execute(argv, envp, path); //child proces is replaced by execve output
+		//cmd1 will be executed by the child and save in pipfd[0]
+		if(dup2(pipefd[0], STDIN_FILENO) < 0) //infile becomes stdin
+		{
+			perror("dup2\n");
+			exit(1);
+		}
+		if(dup2(pipefd[1], STDOUT_FILENO) < 0) //infile becomes stdin
+		{
+			perror("dup2\n");
+			exit(1);
+		}
+		close(pipefd[0]);
+		close(pipefd[1]);						// Done writing, close pipe's write end.
+		execute(cmds, envp, path1); // child proces is replaced by execve output
 	}
 
 	int pid2 = fork();
-	if (pid2 =0 -1)
+	if (pid2 = 0 - 1)
 		return (3);
-	if(pid2 == 0)
+	if (pid2 == 0)
 	{
-		dup2 (ends[1], STDIN_FILENO);
-		close(ends[0]); 
-		close(ends[1]); 
+		execute2(cmds, envp, path1);
+		//use execve again
+		// use file2 and cm2 on stdin?
+		close(pipefd[0]);
+		close(pipefd[1]);
 	}
 
-	
-	// Parent process	
+	// PARENT
 	waitpid(pid1, NULL, 0); // Wait for child.
-	waitpid(pid2, NULL, 0); // Wait for child.
-	//printf("Parent: I received my child. What is its secret ?\n");
-	reader = read(ends[0], buffer, 400);
-	close(ends[0]); // Read everything, close pipe's read end.
+	// waitpid(pid2, NULL, 0); // Wait for child.
+	//execute cmd2 in parent
+
+	reader = read(pipefd[0], buffer, 5);//SET RIGHT AMOUNT OF BYTES
 	if (reader == -1)
-			return (3);
+		return (3);
 	buffer[reader] = '\0';
-	close(ends[1]); // Nothing to write, close pipe's write end.
-	printf("%s\"\n", buffer);
-	
+
+
+
+	close(pipefd[0]);					// Read everything, close pipe's read end.
+	close(pipefd[1]);					// Nothing to write, close pipe's write end.
+	printf("%s\"\n", buffer); // prints output
 	return (0);
 }
 
@@ -121,11 +171,10 @@ int	main(int argc, char **argv, char **envp)
 // for (char **env = envp; *env != 0; env++)
 //   {
 //     char *thisEnv = *env;
-//     printf("%s\n", thisEnv);    
+//     printf("%s\n", thisEnv);
 //   }
 //   return 0;
-	//PATH
-	// if ( == -1)
-	// 	write(1, "error\n", 6);
-	//ft_strnstr
-
+// PATH
+// if ( == -1)
+// 	write(1, "error\n", 6);
+// ft_strnstr
